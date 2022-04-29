adf25bfd736e1af5f1b8f69f54ef61093d0e4b23
Nicola Field <nicola.field2@gmail.com>
2022-04-29 16:56:41 +0100

    adding UI and CSS styling, now it is a playable game

--- script.js
+++ script.js
@@ -2,29 +2,40 @@ let computerSelection = "";
 let userSelection = "";
 let userTotal = 0;
 let computerTotal = 0;
+const instructions = document.querySelector('#instructions');
 const userChoice = document.querySelector('#user-choice');
 const userImage = document.querySelector('#user-image');
 const computerChoice = document.querySelector('#computer-choice')
 const computerImage = document.querySelector('#computer-image');
+const emoji = document.querySelector('.emoji');
 const results = document.querySelector('#results');
 const userRunningTotal = document.querySelector('#user-running-total');
 const computerRunningTotal = document.querySelector('#computer-running-total');
-const instructions = document.querySelector('#instructions');
+const runningTotals = document.querySelector('.running-totals');
+const finalResult = document.querySelector('#final-result');
 const nextRoundButton = document.querySelector('#next-round');
 const playAgainButton = document.querySelector('#play-again')
 const weapons = document.querySelector('#weapons');
+const choices = document.querySelector('.choices');
+const computerChoiceContainer = document.querySelector('#computer-choice-container');
 
 function computerPlay() {
     let randomNumber = Math.floor(Math.random() * 3);
     if (randomNumber === 0) {
         computerSelection = 'rock';
-        computerImage.innerHTML = '<img src="https://images.unsplash.com/photo-1525857597365-5f6dbff2e36e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt="rock">';
+        computerImage.innerHTML = '<span>✊</span>';
+        computerImage.style.visibility = 'visible';
+        computerChoiceContainer.style.visibility = "visible";
     } else if (randomNumber === 1) {
         computerSelection = 'paper';
-        computerImage.innerHTML = '<img src="https://images.unsplash.com/photo-1535350356005-fd52b3b524fb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt="paper">';
+        computerImage.innerHTML = '<span>✋</span>';
+        computerImage.style.visibility = 'visible';
+        computerChoiceContainer.style.visibility = "visible";
     } else {
         computerSelection = 'scissors';
-        computerImage.innerHTML = '<img src="https://images.unsplash.com/photo-1503792501406-2c40da09e1e2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1173&q=80" alt="scissors">';
+        computerImage.innerHTML = '<span>✌️</span>';
+        computerImage.style.visibility = 'visible';
+        computerChoiceContainer.style.visibility = "visible";
     }
     return computerSelection;
 }
@@ -37,10 +48,10 @@ function playRound(userSelection, computerSelection) {
       
       else if (computerSelection === 'rock') {
         if (userSelection === 'paper') {
-            results.textContent = 'Paper beats rock. You win this round';
+            results.textContent = 'Paper beats rock. You get one point';
             return userTotal += 1;
         } else {
-            results.textContent = 'Rock beats scissors. The computer wins this round';
+            results.textContent = 'Rock beats scissors. The computer gets one point';
             return computerTotal += 1;
         };
     }
@@ -48,20 +59,20 @@ function playRound(userSelection, computerSelection) {
 
       else if (computerSelection === 'paper') {
         if (userSelection === 'rock') {
-            results.textContent = 'Paper beats rock. The computer wins this round';
+            results.textContent = 'Paper beats rock. The computer gets one point';
             return computerTotal += 1;
         } else {
-            results.textContent = 'Scissors beats paper. You win this round';
+            results.textContent = 'Scissors beats paper. You get one point';
             return userTotal += 1;
         }
     } 
       
       else if (computerSelection === 'scissors') {
         if (userSelection === 'rock') {
-            results.textContent = 'Rock beats scissors. You win this round';
+            results.textContent = 'Rock beats scissors. You get one point';
             return userTotal += 1;
         } else {
-            results.textContent = 'Scissors beats paper. The computer wins this round';
+            results.textContent = 'Scissors beats paper. The computer gets one point';
             return computerTotal += 1;
         }
     } 
@@ -69,20 +80,20 @@ function playRound(userSelection, computerSelection) {
 
 function game() {
     computerChoice.textContent = `The computer has chosen ${computerSelection}`;
-    userRunningTotal.textContent = `You have ${userTotal} points.`;
-    computerRunningTotal.textContent = `The computer has ${computerTotal} points.`;
+    userRunningTotal.textContent = `You: ${userTotal} points.`;
+    computerRunningTotal.textContent =  `Computer: ${computerTotal} points.`;
         if (userTotal == 3) {
-           instructions.textContent = 'You win the game!';
+           finalResult.textContent = '🎉 You win the game! 🎉';
            playAgainButton.style.display = "block";
            weapons.style.display = "none";
            nextRoundButton.style.display = "none";
         } else if (computerTotal == 3) {
-           instructions.textContent = 'The computer wins the game!'
+           finalResult.textContent = '😭 The computer wins the game! 😭'
            playAgainButton.style.display = "block";
            weapons.style.display = "none";
            nextRoundButton.style.display = "none";
         } else {
-           instructions.textContent = '';
+           finalResult.textContent = '';
         }
 }
 
@@ -91,47 +102,63 @@ function game() {
 
 
 document.getElementById("rock").addEventListener("click", function(){
-        userChoice.textContent = "You have chosen rock";
-        userImage.innerHTML = "<img src=\"https://images.unsplash.com/photo-1525857597365-5f6dbff2e36e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80\">";
+        userChoice.textContent = "You have chosen rock";        
+        userImage.innerHTML = "<span>✊</span>";
         nextRoundButton.style.display = "block";
+        instructions.style.visibility = "hidden";
         computerPlay();
         playRound('rock', computerSelection);
         game();
         weapons.style.display = "none";
+        emoji.style.visibility = "visible";
+        choices.style.visibility = "visible";
+        runningTotals.style.visibility = "visible";
         
 
     })
 document.getElementById("paper").addEventListener("click", function(){
-        userChoice.textContent = "You have chosen paper";
-        userImage.innerHTML = '<img src="https://images.unsplash.com/photo-1535350356005-fd52b3b524fb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt="paper">';
+        userChoice.textContent = "You have chosen paper";        
+        userImage.innerHTML = '<span>✋</span>';
         nextRoundButton.style.display = "block";
+        instructions.style.visibility = "hidden";
         computerPlay();
         playRound('paper', computerSelection);
         game();
         weapons.style.display = "none";
+        emoji.style.visibility = "visible";
+        choices.style.visibility = "visible";
+        runningTotals.style.visibility = "visible";
         
         
     })
 document.getElementById("scissors").addEventListener("click", function(){
         userChoice.textContent = "You have chosen scissors";
-        userImage.innerHTML = '<img src="https://images.unsplash.com/photo-1503792501406-2c40da09e1e2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1173&q=80" alt="scissors">'
+        userImage.innerHTML = '<span>✌️</span>'
         nextRoundButton.style.display = "block";
+        instructions.style.visibility = "hidden";
         computerPlay();
         playRound('scissors', computerSelection);
         game();
         weapons.style.display = "none";
+        emoji.style.visibility = "visible";
+        choices.style.visibility = "visible";
+        runningTotals.style.visibility = "visible";
         
     })
           
 
 function nextRound() {
-    weapons.style.display = "block";
+    weapons.style.display = "flex";
     nextRoundButton.style.display = "none";
     computerChoice.textContent = "";
     results.textContent = "";
     userChoice.textContent = "";
-    userImage.innerHTML = "";
-    computerImage.innerHTML = "";
+    userImage.textContent = "";
+    computerImage.textContent = "";
+    computerImage.style.visibility = "hidden";
+    emoji.style.visibility = "hidden";
+    choices.style.visibility = "hidden";
+    computerChoiceContainer.style.visibility = "hidden";
 }
 
 /* Option to restart match. */
@@ -140,12 +167,18 @@ function playAgain() {
    computerTotal = 0;
    userRunningTotal.textContent ="";
    computerRunningTotal.textContent = "";
-   instructions.textContent = "";
+   finalResult.textContent = "";
    userChoice.textContent = "";
-   userImage.innerHTML = "";
+   userImage.textContent = "";
    computerChoice.textContent = "";
-   computerImage.innerHTML = "";
+   computerImage.textContent = "";
    results.textContent = "";
    playAgainButton.style.display = "none";
-   weapons.style.display = "block";
+   weapons.style.display = "flex";
+   instructions.style.visibility = "visible";
+   computerImage.style.visibility = "hidden";
+   emoji.style.visibility = "hidden";
+   choices.style.visibility = "hidden";
+   computerChoiceContainer.style.visibility = "hidden";
+   runningTotals.style.visibility = "hidden";
 }
